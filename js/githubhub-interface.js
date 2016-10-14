f33816da28ce6d0858e240655afea9555e73e7b3
Jeremy Fryd <fryd21@gmail.com>
2016-10-14 14:09:51 -0700

    reformat mission for app, correspondingly alter interface

--- js/githubhub-interface.js
+++ js/githubhub-interface.js
@@ -2,9 +2,17 @@
 
 $(document).ready(function(){
 
+  $("#launch").click(function(event){
+    $("body").addClass("lucha");
+    $(".risorgimento").show();
+    $(".intro").hide();
+    var audio = document.getElementById("myAudio");
+    // audio.play();
+  });
+
   $("#user-form").submit(function(event){
     event.preventDefault();
     var username = $("#username").val();
-    $("#repositories").text(username);    
+    $("#repositories").text(username);
   });
 });
