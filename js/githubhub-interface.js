0da62203502b68287cebce84cbeebc4f96f3fb1c
Jeremy Fryd <fryd21@gmail.com>
2016-10-14 17:25:10 -0700

    develop template for html stages

--- js/githubhub-interface.js
+++ js/githubhub-interface.js
@@ -3,12 +3,21 @@
 $(document).ready(function(){
 
   $(".launch").click(function(event){
+    event.preventDefault();
+    var audio = document.getElementById("myAudio");
+    // audio.play();
     $(".intro").hide();
     $("body").addClass("lucha");
     $(".risorgimento").show();
     $(".stage-1").show();
-    var audio = document.getElementById("myAudio");
-    audio.play();
+    $("#garibaldi").fadeIn(6000);
+    setTimeout(function(){
+      $("#stage-1-1").fadeIn(1000);
+    }, 6000);
+
+    setTimeout(function(){
+      $("#stage-1-2").fadeIn(1000);
+    }, 8000);
   });
 
   $("#user-form").submit(function(event){
