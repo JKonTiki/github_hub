1e542b6751977862ee6f12365c1dafd7c26f8850
Jeremy Fryd <fryd21@gmail.com>
2016-10-14 17:58:56 -0700

    style speech bubble

--- js/githubhub-interface.js
+++ js/githubhub-interface.js
@@ -5,19 +5,24 @@ $(document).ready(function(){
   $(".launch").click(function(event){
     event.preventDefault();
     var audio = document.getElementById("myAudio");
-    // audio.play();
+    audio.play();
     $(".intro").hide();
-    $("body").addClass("lucha");
-    $(".risorgimento").show();
-    $(".stage-1").show();
-    $("#garibaldi").fadeIn(6000);
     setTimeout(function(){
-      $("#stage-1-1").fadeIn(1000);
-    }, 6000);
+      $("body").addClass("lucha");
 
-    setTimeout(function(){
-      $("#stage-1-2").fadeIn(1000);
-    }, 8000);
+      $(".risorgimento").show();
+      $("#stage-1").show();
+      setTimeout(function(){
+        $("#garibaldi").fadeIn(5000);
+      }, 1000);
+      setTimeout(function(){
+        $("#stage-1-text").fadeIn(1000);
+        $("#stage-1-1").fadeIn(1000);
+      }, 6200);
+      setTimeout(function(){
+        $("#stage-1-2").fadeIn(1000);
+      }, 8300);
+    }, 2000);
   });
 
   $("#user-form").submit(function(event){
